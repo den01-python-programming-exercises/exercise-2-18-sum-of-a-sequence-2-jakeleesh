97e4ebdfdf6c7abc47945bdf42cc8dfc61bc9603
Jake Lee <jake.lee@fdmgroup.com>
2021-09-02 22:14:06 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    first = int(input("First number?"))
+    last = int(input("Last number?"))
+    sum = 0
+    for i in range(first, last + 1):
+        sum += i
+    print("The sum is " + str(sum))
 
 if __name__ == '__main__':
     main()
